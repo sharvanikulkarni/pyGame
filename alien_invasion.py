a86e0d6b753e7992dd784e7ff72989ca6476aca3
Sharvani Kulkarni <sharvani_kulkarni1@baylor.edu>
2022-02-26 19:26:26 -0600

    firing bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     '''overall class to manage game assets and behaviour'''
@@ -18,6 +19,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
         #set the background color
         self.bg_color = (230,230,230)
@@ -27,6 +29,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _check_events(self):
@@ -48,6 +51,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
@@ -55,11 +60,19 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        '''create a new bullet and add it to the bullet group'''
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         #redraw the screen during each pass through the loop.
         self.screen.fill(self.settings.bg_color)
 
         self.ship.blitme()
+
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         #make the most recently drawn screen visible.
         pygame.display.flip()
 
--- bullet.py
+++ bullet.py
@@ -8,7 +8,7 @@ class Bullet(Sprite):
         super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
-        self.color = ai_game.bullet_color
+        self.color = self.settings.bullet_color
 
         '''create a bullet rect at (0,0) and then set correct position'''
         self.rect = pygame.Rect(0,0, self.settings.bullet_width, self.settings.bullet_height)
