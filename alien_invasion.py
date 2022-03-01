2fe28d162fae2d40cbe20ab865b98801331cc1fe
Sharvani Kulkarni <sharvani_kulkarni1@baylor.edu>
2022-03-01 13:23:46 -0600

    mouse hiding

--- alien_invasion.py
+++ alien_invasion.py
@@ -106,6 +106,9 @@ class AlienInvasion:
             self._create_fleet()
             self.ship.center_ship()
 
+            #hide the mouse cursor
+            pygame.mouse.set_visible(False)
+
     def _check_keydown_events(self, event):
         '''respondes to keypresses'''
         if event.key == pygame.K_RIGHT:
@@ -229,6 +232,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _check_aliens_bottom(self):
         '''check if any aliens have reached the bottom of the screen'''
